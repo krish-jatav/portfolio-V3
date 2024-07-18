51182c9344a212a65f359ba87ffbf56714eff4b6
Krish <krishjatav246@gmail.com>
2024-07-18 20:08:19 +0530

    nav bar done

--- src/app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-return(
-    <div className=" h-[80vh] w-full bg-slate-500 flex justify-center items-center">
-        <div className="text-black h-[70vh] w-[90vw] p-10 bg-black">
-            <h1>Let's work together</h1>
-        </div>
-    </div>
-)
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,6 @@
-"use client"
-
-import Image from "next/image";
-import LocomotiveScroll from "locomotive-scroll";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero"
-
 export default function Home() {
-  const locomotiveScroll = new LocomotiveScroll();
   return (
-   <div>
-    <Navbar></Navbar>
-    <Hero></Hero>
-    <Footer></Footer>
-   </div>
-  );
+    <main>
+    </main>
+  )
 }
