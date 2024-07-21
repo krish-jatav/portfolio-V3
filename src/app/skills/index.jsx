bab9ceb26260578fde7a6600cc9020738093e584
Krish <krishjatav246@gmail.com>
2024-07-21 16:06:58 +0530

    add features

--- src/app/skills/index.jsx
+++ src/app/skills/index.jsx
@@ -31,7 +31,7 @@ return(
             <div className="text-slate-50 lg:w-[50vw] ">
                 <div className="  lg:w-[40vw] mt-20 ml-10">
                 <h1 className=" lg:text-7xl text-4xl py-5  ">TOOLS</h1>
-                <p className=" font-thin">I then bring your online vision to life with coding or CMS, depending on your needs. If you don't know what's better for your business, hit me up, I'll be happy to guide you.</p>
+                <p className=" font-thin">I then bring your online vision to life with coding or CMS, depending on your needs. </p>
                 </div>
             </div>
             <div className="lg:border-l-2  ">
